18a60452f6c0fd57fc8486b2491c98815cb932d0
Day-veed <davidadekunle9@gmail.com>
2022-03-23 11:26:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      
+
+      {}
+      <Header/>
+      {}
+      {}
+      {}
+      {}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header'>
+        
+        <div className="header__left">
+            <img src="https://th.bing.com/th/id/R.ab725599921ce3ad5cc1ee2c82f1ee18?rik=7sSeFLtsaFtkcg&pid=ImgRaw&r=0&sres=1&sresct=1" 
+            alt=""/>
+
+            <div className='header__search'>
+                <SearchIcon/>
+                <input type="text"/>
+            </div>
+        </div>
+
+        <div className="header__right">
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
