3ada7d47514a703a7c026eb176f633b1ead14b00
Day-veed <davidadekunle9@gmail.com>
2022-04-20 08:22:12 +0100

    update

--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./Feed.css";
-//import {CreateIcon} from "@mui/material"
+import {CreateIcon} from "@mui/material"
 
 function Feed() {
   return (
     <div className='feed'>
         <div className='feed__inputContainer'>
             <div className='feed_input'>
-                
+                <CreateIcon />
                 <form>
                     <input type="text"/>
                 </form>
