88077bed588adb6d532a5506ed80a94938606a74
Day-veed <davidadekunle9@gmail.com>
2022-04-30 15:24:30 +0100

    3

---
+++ src/features/userSlice.js
@@ -0,0 +1,33 @@
+import {  createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    login: (state, action) => {   
+      state.value = action.payload;
+    },
+    
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+
+});
+
+export const { login, logout } = userSlice.actions;
+
+//selectors
+export const selectUser = (state) => state.user.user;
+
+
+/*export const incrementIfOdd = (amount) => (dispatch, getState) => {
+  const currentValue = selectCount(getState());
+  if (currentValue % 2 === 1) {
+    dispatch(incrementByAmount(amount));
+  }
+};*/
+
+export default userSlice.reducer;
